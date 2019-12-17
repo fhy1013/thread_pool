d3eb4ad844274ada314db212181c5214468fce62
penghonghu <penghonghu@outlook.com>
2019-12-17 11:37:45 +0800

    code format with google-style

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # thread_pool
 c++11 thread pool 
+v1.0
--- src/thread_test.cpp
+++ src/thread_test.cpp
@@ -4,16 +4,18 @@
 #include <iostream>
 #include <chrono>
 
-std::string func1(int x){
-	std::cout<< "---"<<__FUNCTION__ << " " << std::this_thread::get_id()<<"---"<< std::endl;
-	std::this_thread::sleep_for(std::chrono::milliseconds(700));
-	return std::to_string(x * x);
+std::string func1(int x) {
+  std::cout << "---" << __FUNCTION__ << " " << std::this_thread::get_id()
+			<< "---" << std::endl;
+  std::this_thread::sleep_for(std::chrono::milliseconds(700));
+  return std::to_string(x * x);
 }
 
-std::string func2(double d, std::string s){
-	std::cout<< "---"<<__FUNCTION__ << " " << std::this_thread::get_id()<<"---"<< std::endl;
-	std::this_thread::sleep_for(std::chrono::milliseconds(200));
-	return s + std::to_string(d);
+std::string func2(double d, std::string s) {
+  std::cout << "---" << __FUNCTION__ << " " << std::this_thread::get_id()
+			<< "---" << std::endl;
+  std::this_thread::sleep_for(std::chrono::milliseconds(200));
+  return s + std::to_string(d);
 }
 
 void func_test(){
