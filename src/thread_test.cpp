da9baf82805ac5b6e66028a30e17493d64a4e62b
penghonghu <penghonghu@outlook.com>
2020-01-10 17:41:39 +0800

    无返回结果任务

--- src/thread_test.cpp
+++ src/thread_test.cpp
@@ -9,31 +9,33 @@
 std::string func1(int x) {
 	//	std::cout << "---" << __FUNCTION__ << " " << std::this_thread::get_id()
 	//	<< "---" << std::endl;
-	Log(LogLevel::Info, std::this_thread::get_id(), "start...");
-	std::this_thread::sleep_for(std::chrono::milliseconds(300));
+	Log(LogLevel::Info, std::this_thread::get_id(), "start... ", x);
+	std::this_thread::sleep_for(std::chrono::milliseconds(50));
 	Log(LogLevel::Info, std::this_thread::get_id(), "stop");
+	if (x == 70) std::cout << "function completed" << std::endl;
 	return std::to_string(x * x) + " func1";
 }
 
 std::string func2(double d, std::string s) {
 	//	std::cout << "---" << __FUNCTION__ << " " << std::this_thread::get_id()
 	//	<< "---" << std::endl;
-	Log(LogLevel::Info, std::this_thread::get_id(), "start...");
-	std::this_thread::sleep_for(std::chrono::milliseconds(200));
+	Log(LogLevel::Info, std::this_thread::get_id(), "start... ", d, " ", s);
+	std::this_thread::sleep_for(std::chrono::milliseconds(30));
 	Log(LogLevel::Info, std::this_thread::get_id(), "stop...");
+	if (!s.compare("69")) std::cout << "function completed" << std::endl;
 	return s + std::to_string(d) + " func2";
 }
 
-void func_test(){
+void func_test() {
 	ThreadZone::ThreadPool pool;
-	std::vector<std::future<std::string>> res;
+	// std::vector<std::future<std::string>> res;
 
 	Log(LogLevel::Info, std::this_thread::get_id(), "begin...");
 	for (auto i = 0; i < 70; ++i) {
 		if (i % 2 == 0)
-			res.emplace_back(pool.commit(func1, i + 1));
+			pool.commit(func1, i + 1);
 		else
-			res.emplace_back(pool.commit(func2, i + 1, std::to_string(i)));
+			pool.commit(func2, i + 1, std::to_string(i));
 
 		// std::cout<<" thread count: "<<pool.threadCount() << " "<<std::endl;
 		// std::cout<<"idle thread count: " << pool.idlCount() << " "
@@ -45,12 +47,12 @@ void func_test(){
 	}
 
 	Log(LogLevel::Info, "commit all ");
-	// std::cout<< __FUNCTION__ << " commit all "<<std::endl;
+	std::cout<< __FUNCTION__ << " commit all "<<std::endl;
+	Log(LogLevel::Info, "thread count: ", pool.threadCount());
+	pool.setThreadCount(8);
 
-	for (auto &&e : res) {
-		// std::cout << e.get() << ' ';
-		Log(LogLevel::Info, e.get(), " ");
-	}
 	// std::cout << std::endl;
 	Log(LogLevel::Info, std::this_thread::get_id(), "end...");
+	getchar();
+	Log(LogLevel::Info, "thread count: ", pool.threadCount());
 }
